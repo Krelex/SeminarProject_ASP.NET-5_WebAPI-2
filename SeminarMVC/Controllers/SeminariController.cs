c96c9fcd4cdb1ab3beb7a4bf0a3de28f9e1e59ad
Krelex <filip.cog@gmail.com>
2018-07-25 14:07:56 +0200

    Add Seminar action methods and index view

--- SeminarMVC/Controllers/SeminariController.cs
+++ SeminarMVC/Controllers/SeminariController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SeminarMVC.Models;
+using SeminarMVC.Models.REST;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,10 +13,46 @@ namespace SeminarMVC.Controllers
     [Authorize]
     public class SeminariController : Controller
     {
+
+        SeminarREST serviceSeminar = new SeminarREST();
+
         // GET: Seminari
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
+        {
+            var all = await serviceSeminar.GetAllAsync();
+
+            return View(all);
+        }
+
+        public ActionResult Edit(int id)
         {
             return View();
         }
+
+        public ActionResult Edit(Seminar seminar)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Seminar seminar )
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+
     }
 }
